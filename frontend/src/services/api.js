38299937f718abdc64d07b3bbef18794866a3a65
910862KUMAR <145100981+910862KUMAR@users.noreply.github.com>
2026-07-30 13:29:57 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://healthsphere-ai-6.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
